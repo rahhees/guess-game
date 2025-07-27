a516fab1425f649f06e37585d8a4a889f6a2948f
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-07-27 22:14:29 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+import random
+
+while True:
+    number = random.randint(1, 10)
+    guess = input("Guess the number between 1 to 10:\n ")
+
+    if not guess.isdigit():
+        print("Please enter a valid number.")
+        continue
+
+    guess = int(guess)
+
+    if guess == number:
+        print("🎉 You Won!\n")
+    else:
+        print(f"❌ You Lost. The number was {number}\n")
